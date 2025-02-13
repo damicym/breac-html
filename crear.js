831a94076392e5ad7486cf41b4fc6dbdeda91b89
damicym <damiancym05@gmail.com>
2025-02-13 00:44:03 -0300

    mas mejora form

--- crear.js
+++ crear.js
@@ -7,6 +7,10 @@ const añadirCard = card => {
 function function1() {
     document.getElementById('input-pov').click()
 }
+// 
+function function2() {
+    document.getElementById('input-map').click()
+}
 
 function capitalizeFirstLetter(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
@@ -14,8 +18,22 @@ function capitalizeFirstLetter(val) {
 
 var agentes = {}
 
-const fechaHoy = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`
-document.getElementById('fecha-hoy').innerHTML = fechaHoy
+const mostrarFecha = () => {
+    const opcionesfecha = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }
+
+    const fechaHoy = new Date().toLocaleString('es-ES', opcionesfecha).replace(',', '')
+    document.getElementById('fecha-hoy').innerHTML = fechaHoy
+    console.log(fechaHoy)
+}
+
+mostrarFecha()
 
 fetch('data/agentes.json')
     .then(data => data.json())
@@ -47,15 +65,38 @@ document.getElementById("input-pov").addEventListener('input', event => {
     }
     povPreview.src = URL.createObjectURL(povFile)
 })
+// 
+document.getElementById("input-map").addEventListener('input', event => {
+    console.log("document.getElementById")
+    const mapPreview = document.getElementById('map-preview')
+    const mapFileInput = event.target
+    const mapFile = mapFileInput.files[0]
+    if (mapFile) {
+        mapPreview.style.display = 'inline-block'
+        mapPreview.style.border= "3px solid rgb(216, 151, 60)";
+    }
+    else {
+        mapPreview.src = "./images/map-placeholder.png"
+        mapPreview.style.border = "3px solid #273E47"
+        mapPreview.style.opacity= "1"
+    }
+    mapPreview.src = URL.createObjectURL(mapFile)
+})
 
 document.getElementById('form-crear').addEventListener('submit', event => {
     event.preventDefault()
     const formData = new FormData(event.target)
     const formObject = Object.fromEntries(formData.entries())
-
+    //--
+    const mapPreview = document.getElementById('map-preview')
+    const mapFileInput = event.target
+    const mapFile = mapFileInput.files[0]
+    let img = document.getElementById("pov-preview")
+    
     localStorage.setItem('form', JSON.stringify(formObject))
     let titulo = formData.get('title')
 })
+
 
 document.getElementById('agente').addEventListener('change', event => {
     console.log('agente', event.target.value)
