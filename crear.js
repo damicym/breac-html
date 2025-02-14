b24b37d7b4860db21ae8764f6ee747ad0b058311
damicym <damiancym05@gmail.com>
2025-02-14 01:54:12 -0300

    form terminado

--- crear.js
+++ crear.js
@@ -1,19 +1,20 @@
-const añadirCard = card => {
-    const cards = localStorage.getItem('cards') || []
-    cards.push(card)
-    localStorage.setItem('cards', JSON.stringify(cards))
-}
+//mis elementos:
+const selectHabilidad = document.getElementById('habilidad')
+const povPreview = document.getElementById('pov-preview')
+const mapPreview = document.getElementById('map-preview')
+const inputHabilidad = document.getElementById('habilidad')
+const formCrear = document.getElementById('form-crear')
 
 function function1() {
     document.getElementById('input-pov').click()
 }
-// 
+
 function function2() {
     document.getElementById('input-map').click()
 }
 
 function capitalizeFirstLetter(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1)
 }
 
 var agentes = {}
@@ -30,7 +31,7 @@ const mostrarFecha = () => {
 
     const fechaHoy = new Date().toLocaleString('es-ES', opcionesfecha).replace(',', ' •')
     document.getElementById('fecha-hoy').innerHTML = fechaHoy
-    
+
 }
 
 mostrarFecha()
@@ -39,10 +40,9 @@ fetch('data/agentes.json')
     .then(data => data.json())
     .then(data => {
         agentes = data
-        localStorage.setItem('test', JSON.stringify(agentes))
-        // <option value="astra">Astra</option>
+
         agentes.map(agente => {
-            let opcion = `<option value="${agente.value}">${capitalizeFirstLetter(agente.value)}</option>`
+            let opcion = `<option value="${agente.nombre}">${capitalizeFirstLetter(agente.nombre)}</option>`
             document.getElementById('agente').innerHTML += opcion
         })
     })
@@ -50,42 +50,41 @@ fetch('data/agentes.json')
 // JSON.stringify(dataEnJSON) --> Devuelve un string
 // JSON.parse(string) --> Devuelve un JSON
 
-function uncapitalize(str){
-    return str.charAt(0).toLowerCase() + str.slice(1);
+function uncapitalize(str) {
+    return str.charAt(0).toLowerCase() + str.slice(1)
 
 }
 
 document.getElementById('agente').addEventListener('change', event => {
-    const agenteSeleccionado = event.target.value;
-    const agentes = JSON.parse(localStorage.getItem('test')) || [];
-    const indice = agentes.findIndex(obj => obj.value === agenteSeleccionado);
-    const agente = agentes[indice];
+    const agenteSeleccionado = event.target.value
+    const indice = agentes.findIndex(obj => obj.nombre === agenteSeleccionado)
+    const agente = agentes[indice]
+
     
-    const selectHabilidad = document.getElementById('habilidad');
-    selectHabilidad.innerHTML = "";
-    
-    selectHabilidad.insertAdjacentHTML('beforeend', '<option value="">Elige una habilidad</option>');
-    let opcion1 = `<option value="${agente.h1}">${capitalizeFirstLetter(agente.h1)}</option>`;
-    selectHabilidad.insertAdjacentHTML('beforeend', opcion1);
+    selectHabilidad.innerHTML = ""
 
-    let opcion2 = `<option value="${agente.h2}">${capitalizeFirstLetter(agente.h2)}</option>`;
-    selectHabilidad.insertAdjacentHTML('beforeend', opcion2);
+    selectHabilidad.insertAdjacentHTML('beforeend', '<option value="">Elige una habilidad</option>')
+    let opcion1 = `<option value="${agente.habilidades.h1}">${capitalizeFirstLetter(agente.habilidades.h1)}</option>`
+    selectHabilidad.insertAdjacentHTML('beforeend', opcion1)
 
-    let opcion3 = `<option value="${agente.h3}">${capitalizeFirstLetter(agente.h3)}</option>`;
-    selectHabilidad.insertAdjacentHTML('beforeend', opcion3);
+    let opcion2 = `<option value="${agente.habilidades.h2}">${capitalizeFirstLetter(agente.habilidades.h2)}</option>`
+    selectHabilidad.insertAdjacentHTML('beforeend', opcion2)
 
-    let opcion4 = `<option value="${agente.h4}">${capitalizeFirstLetter(agente.h4)}</option>`;
-    selectHabilidad.insertAdjacentHTML('beforeend', opcion4);
+    let opcion3 = `<option value="${agente.habilidades.h3}">${capitalizeFirstLetter(agente.habilidades.h3)}</option>`
+    selectHabilidad.insertAdjacentHTML('beforeend', opcion3)
 
-    if (agente.h5) {
-        let opcion5 = `<option value="${agente.h5}">${capitalizeFirstLetter(agente.h5)}</option>`;
-        selectHabilidad.insertAdjacentHTML('beforeend', opcion5);
+    let opcion4 = `<option value="${agente.habilidades.h4}">${capitalizeFirstLetter(agente.habilidades.h4)}</option>`
+    selectHabilidad.insertAdjacentHTML('beforeend', opcion4)
+
+    if (agente.habilidades.h5) {
+        let opcion5 = `<option value="${agente.habilidades.h5}">${capitalizeFirstLetter(agente.habilidades.h5)}</option>`
+        selectHabilidad.insertAdjacentHTML('beforeend', opcion5)
     }
-});
+})
 
 
 document.getElementById("input-pov").addEventListener('input', event => {
-    const povPreview = document.getElementById('pov-preview')
+    
     const povFileInput = event.target
     const povFile = povFileInput.files[0]
     if (povFile) {
@@ -99,14 +98,14 @@ document.getElementById("input-pov").addEventListener('input', event => {
     }
     povPreview.src = URL.createObjectURL(povFile)
 })
-// 
+
 document.getElementById("input-map").addEventListener('input', event => {
-    const mapPreview = document.getElementById('map-preview')
+    
     const mapFileInput = event.target
     const mapFile = mapFileInput.files[0]
     if (mapFile) {
         mapPreview.style.display = 'inline-block'
-        mapPreview.style.border = "3px solid rgb(216, 151, 60)";
+        mapPreview.style.border = "3px solid rgb(216, 151, 60)"
     }
     else {
         mapPreview.src = "./images/map-placeholder.png"
@@ -116,23 +115,42 @@ document.getElementById("input-map").addEventListener('input', event => {
     mapPreview.src = URL.createObjectURL(mapFile)
 })
 
-document.getElementById('form-crear').addEventListener('submit', event => {
+document.getElementById('agente').addEventListener('change', event => {
+    
+    inputHabilidad.disabled = !event.target.value
+    if (!event.target.value) inputHabilidad.value = ""
+})
+function resetPreviewPov() {
+    
+    povPreview.src = "./images/pov-placeholder.png"
+    povPreview.style.border = "3px solid #273E47"
+    povPreview.style.borderRadius = "10px"
+}
+function resetPreviewMap() {
+    
+    mapPreview.src = "./images/map-placeholder.png"
+    mapPreview.style.border = "3px solid #273E47"
+    mapPreview.style.opacity = "1"
+}
+function resetHabilidad(){
+    inputHabilidad.disabled = true
+    inputHabilidad.value = ""
+}
+function vaciarInput(array) {
+    formCrear.reset()
+    resetPreviewPov()
+    resetPreviewMap()
+    resetHabilidad()
+}
+
+
+formCrear.addEventListener('submit', event => {
     event.preventDefault()
     const formData = new FormData(event.target)
     const formObject = Object.fromEntries(formData.entries())
-    //--
-    const mapPreview = document.getElementById('map-preview')
-    const mapFileInput = event.target
-    const mapFile = mapFileInput.files[0]
-    let img = document.getElementById("pov-preview")
-
-    localStorage.setItem('form', JSON.stringify(formObject))
-    let titulo = formData.get('title')
-})
-
-
-document.getElementById('agente').addEventListener('change', event => {
-    const inputHabilidad = document.getElementById('habilidad')
-    inputHabilidad.disabled = !event.target.value
-    if (!event.target.value) inputHabilidad.value = ""
+    const keysArray = [...formData.keys()]
+    vaciarInput(keysArray)
+    
+    //localStorage.setItem('form', JSON.stringify(formObject))
+    // para el script de habilidades let titulo = formData.get('title')
 })
