7d1f1289868b0b98cd6d3e6d6111f1d7307b4e22
damicym <damiancym05@gmail.com>
2025-02-16 20:26:37 -0300

    diseño filtros

--- habilidades.js
+++ habilidades.js
@@ -17,6 +17,9 @@ const maxContainer = document.getElementById("maxContainer")
 // JSON.stringify(dataEnJSON) --> Devuelve un string
 // JSON.parse(string) --> Devuelve un JSON
 
+//filtros:
+//1. guardar en el localstorage los filtros mapas = [], agente, habilidad, autor
+//2. generar las cards filtradas
 fetch('data/agentes.json')
     .then(data => data.json())
     .then(data => {
@@ -24,79 +27,45 @@ fetch('data/agentes.json')
         fetch('data/cards.json')
             .then(data => data.json())
             .then(data => {
+                var cards = data
                 let localData = []
                 for (let index = 0; index < localStorage.length; index++) {
                     let key = localStorage.key(index)
-                    if (key.startsWith("form")) { 
+                    if (key.startsWith("form")) {
                         let formData = JSON.parse(localStorage.getItem(key)) || []
                         localData.push(formData)
                     }
                 }
-                var allCards = [...data, ...localData]
-                // cards = data + JSON.parse(localStorage.getItem('form'))
-                agentes.map(agente => {
-                    const agenteCards = allCards.filter(card => card.agente === agente.nombre)
-                    if (agenteCards.length > 0) {
-                        maxContainer.innerHTML += `<h2 class="mapa-titulo">${capitalizeFirstLetter(agente.nombre)}</h2>
-                            <div class="card-container" id="${agente.nombre}-container"></div>`
-                        const agenteContainer = document.getElementById(`${agente.nombre}-container`)
-                        agenteCards.map((card, index) => {
-                            let cardHtml = `<article class="card">
-                                            <p class="title">
-                                                <span class="title-num">${index + 1}</span>
-                                                <span class="title-text">${card.titleText}</span>
-                                            </p>
-                                            <img class="map" src="${card.mapSrc}">
-                                            <img class="pov" src="${card.povSrc}">
-                                            <p class="desc">${card.desc}</p>
-                                           <div class="firma">
-                                                <span class="autor">${card.nombre} •</span>
-                                                <span class="fecha-card">${card.fecha}</span>
-                                           </div>
-                                        </article>`
-                            agenteContainer.innerHTML += cardHtml
-                        })
-                    }
-                })
+                var allCards = [...cards, ...localData]
+                generarCards(allCards, agentes)
+
             })
     })
 
 
-
-// fetch('data/cards.json')
-//     .then(data => data.json())
-//     .then(data => {
-//         cards = data
-
-//         const breachCards = cards.filter(card => card.agente === "breach")
-//         if (breachCards.length > 0) {
-//             var num = 1
-//             breachCards.map(card => {
-//                 let cardHtml = `<article class="card">
-//             <p class="title">
-//                 <span class="title-num">${num}</span>
-//                 <span class="title-text">${card.titleText}</span>
-//             </p>
-//             <img class="map" src="${card.mapSrc}">
-//             <img class="pov" src="${card.povSrc}">
-//             <p class="desc">${card.desc}</p>
-//            <div class="firma">
-//                 <span class="autor">${card.nombre} •</span>
-//                 <span class="fecha-card">${card.fecha}</span>
-//            </div>
-//         </article>`
-//                 breachContainer.innerHTML += cardHtml
-//                 num++
-//             })
-//         }
-
-
-//     })
-// switch (card.agente) {
-//     case "breach":
-//         breachContainer.innerHTML += cardHtml
-//         break
-//     case "cypher":
-//         cypherContainer.innerHTML += cardHtml
-//         break
-// }
+function generarCards(cards, agentes) {
+    agentes.map(agente => {
+        const agenteCards = cards.filter(card => card.agente === agente.nombre)
+        if (agenteCards.length > 0) {
+            maxContainer.innerHTML += `<h2 class="mapa-titulo">${capitalizeFirstLetter(agente.nombre)}</h2>
+                <div class="card-container" id="${agente.nombre}-container"></div>`
+            const agenteContainer = document.getElementById(`${agente.nombre}-container`)
+            agenteCards.map((card, index) => {
+                let cardHtml = `<article class="card">
+                                <p class="title">
+                                    <span class="title-num">${index + 1}</span>
+                                    <span class="title-text">${card.titleText}</span>
+                                </p>
+                                <img class="map" src="${card.mapSrc}">
+                                <img class="pov" src="${card.povSrc}">
+                                <p class="desc">${card.desc}</p>
+                               <div class="firma">
+                                    <span class="autor">${card.nombre} •</span>
+                                    <span class="fecha-card">${card.fecha}</span>
+                               </div>
+                            </article>`
+                agenteContainer.innerHTML += cardHtml
+            })
+        }
+    })
+}
